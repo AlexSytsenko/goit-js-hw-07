0a24e2b9353f319ca87296fdc75fd9bf09b1e824
AlexSytsenko <sytsenko@gmail.com>
2020-12-20 19:03:16 +0200

    Подпраляет задачу №1

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,19 @@
-const categoriesRef = document.querySelector("#categories"),
+//Исправил после обратной связи ментора.
+
+const categoriesRef = document.querySelectorAll(".item");
+
+console.log(`В списке ${categoriesRef.length} категории.`);
+
+categoriesRef.forEach((item) => {
+  const title = item.querySelector("h2").textContent;
+  const quantityItems = item.querySelectorAll("ul li").length;
+
+  console.log(`Категория: ${title}\nКоличество элементов: ${quantityItems}`);
+});
+
+//Мое первое решение сделал так!
+
+/*const categoriesRef = document.querySelector("#categories"),
   itemsCategories = [...categoriesRef.children];
 
 console.log(`В списке ${itemsCategories.length} категории.`);
@@ -9,3 +24,4 @@ itemsCategories.forEach((item) => {
 
   console.log(`Категория: ${title}\nКоличество элементов: ${quantityItems}`);
 });
+*/

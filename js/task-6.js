a75d42ad9559346779bcbf34902ae52431af1b56
AlexSytsenko <sytsenko@gmail.com>
2020-12-20 18:39:22 +0200

    исправляет 6 задачу

--- js/task-6.js
+++ js/task-6.js
@@ -1,18 +1,15 @@
 const inputRef = document.querySelector("#validation-input");
 
-const dataValue = +inputRef.dataset.length;
+const dataValue = Number(inputRef.dataset.length);
 
 inputRef.addEventListener("blur", handleInput);
-inputRef.addEventListener("focus", removeClass);
 
 function handleInput() {
   if (event.target.value.length !== dataValue) {
     inputRef.classList.add("invalid");
+    inputRef.classList.remove("valid");
+  } else {
+    inputRef.classList.add("valid");
+    inputRef.classList.remove("invalid");
   }
-  inputRef.classList.add("valid");
-}
-
-function removeClass() {
-  inputRef.classList.remove("valid");
-  inputRef.classList.remove("invalid");
 }
